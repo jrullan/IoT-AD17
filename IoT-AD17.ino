096c90c96d8db3159a13e0630fd898b729e16c0d
Jose Rullan <jerullan@yahoo.com>
2017-03-25 20:01:11 -0400

    First commit of IoT code

---
+++ IoT-AD17.ino
@@ -0,0 +1,127 @@
+#include <ESP8266WiFi.h>
+#include <neotimer.h>
+
+#define BUTTON D0
+#define LED D1
+
+Neotimer blinker = Neotimer(500);
+Neotimer timeout = Neotimer(5000);
+Neotimer debouncer = Neotimer(3000);
+
+WiFiClient client;
+
+const char* ssid     = "frulrom2319";
+const char* password = "familiarullan2319";
+
+// Carriots Integration
+const char* host = "api.carriots.com";
+const char* DEVICE   = "defaultDevice@jrullan.jrullan";
+const char* APIKEY = "ff932a7071fac2aa3f0a4005945e1feba1a8cf6269cf014d47d09bc0a2563ed9";
+
+#include "carriots.h"
+
+void setup() {
+  pinMode(LED,OUTPUT);
+  pinMode(BUTTON,INPUT);
+  
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop() {
+  /*
+   * Allow ESP8266 to perform background tasks
+   */
+  yield();
+
+  /*
+   * Blink led at 1 Hz @ 50% duty cycle
+   */
+  if(blinker.repeat(500)){
+    digitalWrite(LED,!digitalRead(LED));
+  }
+
+  /*
+   * Checks if button is pressed and timer is not running
+   */
+  if(digitalRead(BUTTON)==HIGH && !debouncer.waiting()){
+    debouncer.start();    
+    Serial.println("button pressed");
+    sendStream("button",false);
+    sendStream("switch",2);
+    sendStream("name","Jose");
+    sendStream("level",50.5);
+  }
+
+  /*
+   * Resets timer after it finishes
+   */
+  if(debouncer.done()){
+    debouncer.stop();
+    Serial.println("debouncer timer reset");
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+}
+
+/*
+void sendStream(){
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+ 
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  
+  // While not available
+  while (client.available() == 0) {
+    timeout.start();
+    if (timeout.done()) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+  
+}
+*/
---
+++ carriots.h
@@ -0,0 +1,114 @@
+/*
+ * This function creates the message body
+ * and returns it as a String
+ * attributeName - name of the attribute
+ * value - value of the attribute
+ * type - type of the attribute (1-String, 2-Integer, 3-Float)
+ */
+String messageBody(String attributeName, String value, int type){
+  String body = "{\"protocol\":\"v2\",";
+  body += "\"checksum\":\"\",";
+  body += "\"device\":\"";
+  body += DEVICE;
+  body += "\",";
+  body += "\"at\":\"now\",";
+  body += "\"data\":{\"";
+  body += attributeName;
+  body += "\":";
+  switch(type){
+    case 1: 
+      body += "\"";
+      body += value;
+      body += "\"";
+      break;
+    case 2:
+      body += value.toInt();
+      break;
+    case 3:
+      body += value.toFloat();
+      break;
+    default:
+      body += value;
+  }
+  body += "}}";
+  return body;
+}
+/*
+ * This function builds the Request header
+ */
+String request(String body){
+  String header = "POST /streams/ HTTP/1.1\r\n";
+  header += "Host: ";
+  header += host;
+  header += "\r\n";
+  header += "Accept: application/json\r\n";
+  header += "User-Agent: ";
+  header += DEVICE;
+  header += "\r\n";
+  header += "Content-Type: application/json\r\n";
+  header += "Content-Length: ";
+  header += body.length();
+  header += "\r\n";
+  header += "carriots.apikey: ";
+  header += APIKEY;
+  header += "\r\n";
+  return header;
+}
+
+/*
+ * This function sends the Http request
+ * with the data
+ */
+void httpSend(String header,String body){
+  if(client.connect(host,80)){
+    client.print(header);
+    client.print("\r\n");
+    client.println(body);
+  }else{
+    Serial.println("connection failed");
+  }
+}
+
+/*
+ * This function sends a stream to the carriots server
+ * attributeName - A name in "quotes" for the attribute
+ * value - A String value for the attribute
+ */
+void sendStream(String attributeName, String value){
+  String json = messageBody(attributeName,value,1);
+  String header = request(json);
+  Serial.print(header);
+  Serial.print("\r\n");
+  Serial.println(json); 
+  httpSend(header,json);
+}
+/*
+ * This function sends a stream to the carriots server
+ * attributeName - A name in "quotes" for the attribute
+ * value - An int value for the attribute
+ */
+void sendStream(String attributeName, int value){
+  String json = messageBody(attributeName,String(value),2);
+  String header = request(json);
+  Serial.print(header);
+  Serial.print("\r\n");
+  Serial.println(json); 
+  httpSend(header,json);
+}
+/*
+ * This function sends a stream to the carriots server
+ * attributeName - A name in "quotes" for the attribute
+ * value - A float value for the attribute
+ */
+void sendStream(String attributeName, double value){
+  String json = messageBody(attributeName,String(value),3);
+  String header = request(json);
+  Serial.print(header);
+  Serial.print("\r\n");
+  Serial.println(json); 
+  httpSend(header,json);
+}
+
+
+
+
